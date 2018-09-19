811df377e41823850fdbce65f499a6f679543909
ianfreitas1 <ianfreitas_2005@hotmail.com>
2018-09-19 16:17:28 -0300

    main

--- main.cpp
+++ main.cpp
@@ -15,25 +15,25 @@ int main(){
   cout << "nome do arquivo: " << endl;
   cin >> arquivo;
   cout << "Carregando..." << endl;
-  int opcao;
-  cout << "1: Grau" << endl;
-  cout << "2: BFS" << endl;
-  cout << "3: DFS" << endl;
-  cout << "4: CC" << endl;
-  cin >> opcao;
   //Matriz matriz = Matriz(arquivo);
   //matriz.BFS(1);
   //int vizin;
   //cin >> vizin;
   //matriz.vizinhos(1);
   Lista lista = Lista(arquivo);
+  int opcao;
+  cout << "1: Grau" << endl;
+  cout << "2: BFS" << endl;
+  cout << "3: DFS" << endl;
+  cout << "4: CC" << endl;
+  cin >> opcao;
   switch(opcao){
     case 1:
       lista.Grau2();
       break;
     case 2:
       int i;
-      cout << "Escolha vertice da BFS: ";
+      cout << "Escolha vértice da BFS: ";
       cin >> i;
       lista.BFS(i);
       break;
